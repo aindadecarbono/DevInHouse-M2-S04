595d64573c55fa7a310262a5b540514f00917d6c
Bruno Coelho <brunocoelhonet@gmail.com>
2022-06-28 17:19:06 -0300

    Ex1S4

---
+++ FichaInscricao.cs
@@ -0,0 +1,21 @@
+namespace DevInHouse;
+
+public class FichaInscricao
+{
+
+  public string Nome { get; set; }
+
+  public int Idade { get; set; }
+
+  public string Curso { get; set; }
+
+  public string Escolaridade { get; set; }
+
+  public DateTime DataNascimento { get; set; }
+
+  public decimal ValorCurso { get; set; }
+
+  public decimal ValorDesconto { get; set; }
+
+  public decimal ValorMulta { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace DevInHouse;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+
+  }
+}
