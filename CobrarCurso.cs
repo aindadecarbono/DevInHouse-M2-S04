45dae1a55e6135dc0e337f256befda64bb3aa6ca
Bruno Coelho <brunocoelhonet@gmail.com>
2022-06-29 17:04:22 -0300

    Ex7

--- CobrarCurso.cs
+++ CobrarCurso.cs
@@ -5,4 +5,14 @@ public class CobrarCurso
   private double ValorCurso { get; set; }
   private double ValorMulta { get; set; }
   private double ValorDesconto { get; set; }
+
+  private double Resultado { get; set; }
+
+  private void Calculo()
+  {
+    if (ValorMulta > 0)
+      Resultado = ValorCurso + ValorMulta;
+    else if (ValorMulta == 0)
+      Resultado = ValorCurso - ValorDesconto;
+  }
 }
