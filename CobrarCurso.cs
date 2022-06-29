c8e016e1850622de4f5c33323b715f6098b1c13b
Bruno Coelho <brunocoelhonet@gmail.com>
2022-06-29 16:54:46 -0300

    Ex6S4

---
+++ CobrarCurso.cs
@@ -0,0 +1,8 @@
+namespace DevInHouse;
+
+public class CobrarCurso
+{
+  private double ValorCurso { get; set; }
+  private double ValorMulta { get; set; }
+  private double ValorDesconto { get; set; }
+}
